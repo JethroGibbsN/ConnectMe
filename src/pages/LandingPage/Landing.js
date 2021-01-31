aae7bc3fa01191c43ef040b47a20dafc55e9aed6
alsalemBayan <alsalembayan96@gmail.com>
2021-01-31 05:01:00 -0500

    landingPage

--- src/pages/LandingPage/Landing.js
+++ src/pages/LandingPage/Landing.js
@@ -1,9 +1,11 @@
 import react from "react";
 import "./Landing.css";
 import hand from "../img/hands.PNG";
-import box1 from "../img/box1.PNG";
-import box2 from "../img/box2.PNG";
-import box3 from "../img/box3.PNG";
+// import box1 from "../img/box1.PNG";
+// import box2 from "../img/box2.PNG";
+// import box3 from "../img/box3.PNG";
+import capture from "../img/Capture.PNG";
+import join from "../img/join.PNG";
 
 function LandingPage() {
   return (
@@ -27,17 +29,23 @@ function LandingPage() {
         <img className="img1" src={hand}></img>
       </div>
 
-      <div className="flex3">
+      {/* <div className="flex3">
         <img src={box1}></img>
         <img src={box2}></img>
         <img src={box3}></img>
-      </div>
+      </div> */}
 
-      <div className="aboutUs">
+      {/* <div className="center">
+        <button className="paragraph">
+          Search for Interests and Find a Mutual Friend{" "}
+        </button>
+      </div> */}
+      <img className="join" src={join}></img>
+      <img src={capture}></img>
+
+      <div>
         <h2 className="h22">About Us</h2>
-        <p>
-          <i>ConnectMe</i>
-        </p>
+
         <p>
           With the evolving situation during the pandemic, people are facing
           many challenges. Such as socializing with others. It is getting very
