0ed132cbad24ad42ec7c3d6021792ae8113f9123
APiligrim <anastasiyauraleva@gmail.com>
2021-01-31 00:06:48 -0500

    added navbar etc.

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,11 +1,12 @@
 import react from 'react';
-import SiderDemo from  '../layout/Layout.js';
+
 import './Home.css'
 function HomePage() {
     return (
       <div className="HomePage">
-
-          <div> <h1>This is a Home/Activities Page Hello</h1></div>
+          <div> 
+            <h1>This is a Home/Activities Page Hello</h1>
+          </div>
       </div>
     );
   }
