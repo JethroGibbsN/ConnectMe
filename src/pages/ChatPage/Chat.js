79e454be06266c4719030746eee2a7c0859176d6
krixstin <kristinhajinkim@gmail.com>
2021-01-31 01:43:47 -0500

    chessplayers

--- src/pages/ChatPage/Chat.js
+++ src/pages/ChatPage/Chat.js
@@ -9,15 +9,46 @@ import {
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons';
-
+import './Chat.css'
+import profile1 from '../img/profile1.png'
+import profile2 from '../img/profile2.png'
+import profile3 from '../img/profile3.png'
+import profile4 from '../img/profile4.png'
 
 function ChatPage() {
     return (
       <div className="ChatPage">
-          <div> This is a Chat Page HELLLOO</div>
+
+          <div id='chessplayer_title'>
+            Chess Players Online
+          </div>
+
+          <div id="container">
+            
+            <div class="profile">
+              <img src={profile1} style={{width:"321px", height:"435px"}}></img>
+              <button class="btn">Connect</button>
+            </div>
+            <div class="profile">
+              <img src={profile2} style={{width:"321px", height:"435px"}}></img>
+              <button class="btn">Connect</button>
+            </div>
+            <div class="profile">
+              <img src={profile3} style={{width:"321px", height:"435px"}}></img>
+              <button class="btn">Connect</button>
+            </div>
+            <div class="profile">
+              <img src={profile4} style={{width:"321px", height:"435px"}}></img>
+              <button class="btn">Connect</button>
+            </div>
+            
+            
+
+          </div>
+
+
+
       </div>
-
-
     );
   }
   
